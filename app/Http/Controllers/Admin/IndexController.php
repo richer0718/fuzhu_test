06cab4b73b4d1ec4b63604182efd1d333c338965
杨晓杰 <1127129489@qq.com>
2017-12-16 20:39:47 +0800

    debug

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -11,7 +11,7 @@ class IndexController extends Controller
 {
     //
     public function login(){
-        return view('zyfanddzy/laoban');
+        return view('admin/login');
 
     }
     public function loginout(Request $request){
