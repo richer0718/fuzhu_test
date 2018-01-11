267d274d3d97ee8dc79956bd838edd0e654d5f9e
杨晓杰 <1127129489@qq.com>
2018-01-11 20:53:34 +0800

    update images

--- resources/views/kefu/index.blade.php
+++ resources/views/kefu/index.blade.php
@@ -90,10 +90,10 @@
                             <td>{{$vo -> map}}</td>
                             <td>{{$vo -> mode}}</td>
                             <td>
-                                @if($vo -> status == '微信二维码') <a href="{{ 'http://img.feisushouyou.com/jietu/'.$vo->area.'-'.$vo->number.'.jpg' }}" target="_blank" style="color:red;">扫描微信二维码</a>
-                                @elseif($vo -> status == '手机验证码' )  <a class="yanzhengma" data="{{ $vo -> id }}" style="color:red;">输入验证码</a> <a href="{{ 'http://img.feisushouyou.com/jietu/'.$vo->area.'-'.$vo->number.'.jpg' }}" target="_blank" style="color:red;">查看图片</a>
-                                @elseif($vo -> status == '正常刷完' ) <a href="{{ 'http://img.feisushouyou.com/jietu/'.$vo->area.'-'.$vo->number.'-wc.jpg' }}" target="_blank" style="color:red;">正常刷完</a>
-                                @elseif($vo -> status == '区不存在' ) <a href="{{ 'http://img.feisushouyou.com/jietu/'.$vo->area.'-'.$vo->number.'-xq.jpg' }}" target="_blank" style="color:red;">区不存在</a>
+                                @if($vo -> status == '微信二维码') <a href="{{ 'http://feisushouyou.com/fuzhu/public/images/'.$vo->area.'-'.$vo->number.'.jpg' }}" target="_blank" style="color:red;">扫描微信二维码</a>
+                                @elseif($vo -> status == '手机验证码' )  <a class="yanzhengma" data="{{ $vo -> id }}" style="color:red;">输入验证码</a> <a href="{{ 'http://feisushouyou.com/fuzhu/public/images/'.$vo->area.'-'.$vo->number.'.jpg' }}" target="_blank" style="color:red;">查看图片</a>
+                                @elseif($vo -> status == '正常刷完' ) <a href="{{ 'http://feisushouyou.com/fuzhu/public/images/'.$vo->area.'-'.$vo->number.'-wc.jpg' }}" target="_blank" style="color:red;">正常刷完</a>
+                                @elseif($vo -> status == '区不存在' ) <a href="{{ 'http://feisushouyou.com/fuzhu/public/images/'.$vo->area.'-'.$vo->number.'-xq.jpg' }}" target="_blank" style="color:red;">区不存在</a>
                                 @else{{$vo -> status}}
                                 @endif
                             </td>
