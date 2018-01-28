5f0a1dd3ff60e1616f22c41bfc5eaddb8f228610
杨晓杰 <1127129489@qq.com>
2018-01-28 17:07:27 +0800

    debug

--- resources/views/layouts/common_admin.blade.php
+++ resources/views/layouts/common_admin.blade.php
@@ -1,7 +1,5 @@
 <meta charset="utf-8">
 <meta name="renderer" content="webkit">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="{{asset('admin/css/bootstrap.min.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('admin/css/style.css')}}">
 <link rel="stylesheet" type="text/css" href="{{asset('admin/css/font-awesome.min.css')}}">
